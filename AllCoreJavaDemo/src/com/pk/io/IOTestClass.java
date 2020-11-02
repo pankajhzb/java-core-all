7ac66adafce36af7f8c13b46939baa53e17c748c
Pankaj Kumar <pankaj.kumar68@ad.infosys.com>
2020-11-02 07:46:38 +0530

    algo

--- AllCoreJavaDemo/src/com/pk/io/IOTestClass.java
+++ AllCoreJavaDemo/src/com/pk/io/IOTestClass.java
@@ -1,52 +1,32 @@
 package com.pk.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class IOTestClass {
+	public static void main(String[] args) throws IOException {
+		useBufferedReader();
+		// useScanner();
+	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		 /* Sample code to perform I/O:
-         * Use either of these methods for input
-
-        //BufferedReader
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String name = br.readLine();                // Reading input from STDIN
-        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-*/
-        //Scanner
-        Scanner s = new Scanner(System.in);
-        String name = s.nextLine();                 // Reading input from STDIN
-        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-
-        
-
-        // Write your code herePranjal
+	private static void useScanner() {
+		Scanner s = new Scanner(System.in);
+		String number = s.nextLine();
+		System.out.println("Count, " + number + ".");
+		String name = s.nextLine();
+		System.out.println("Hi, " + name + ".");
 
 	}
 
-}
+	private static void useBufferedReader() throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		String number = br.readLine();
+		System.out.println("Count, " + number + ".");
 
-
-/* IMPORTANT: Multiple classes and nested static classes are supported */
-
-/*
- * uncomment this if you want to read input.
-//imports for BufferedReader
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-*/
-//import java.//for Scanner and other utility classes
-//import java.util.*;
-
-
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-
-class TestClass {
-    public static void main(String args[] ) throws Exception {
-       
-
-    }
+		String nameread = br.readLine();
+		System.out.println("Hi, " + nameread + ".");
+	}
 }
